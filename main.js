1fb33ce5cd9e417c9b7db4336092451287742d5b
Rémi MARTINEZ <remi.martinez-castro@sncf.fr>
2023-09-06 11:36:18 +0200

    add sch

--- main.js
+++ main.js
@@ -16,12 +16,14 @@ document.querySelector('#app').innerHTML = `
                 <img id="lDamso" src="img/damso.png" class="selected" title="Damso" alt="Damso">
                 <img id="lOrelsan" src="img/orelsan.png" title="Orelsan" alt="Orelsan">
                 <img id="lNekfeu" src="img/nekfeu.png" title="Nekfeu" alt="Nekfeu">
+                <img id="lSCH" src="img/sch.png" title="SCH" alt="SCH">
             </div>
             <button class="btn">Générer</button>
             <div class="rightchoice">
                 <img id="rDamso" class="imgflip" src="img/damso.png" title="Damso" alt="Damso">
-                <img id="rOrelsan" class="imgflip selected" src="img/orelsan.png" title="Damso" alt="Damso">
-                <img id="rNekfeu" class="imgflip" src="img/nekfeu.png" title="Damso" alt="Damso">
+                <img id="rOrelsan" class="imgflip selected" src="img/orelsan.png" title="Orelsan" alt="Orelsan">
+                <img id="rNekfeu" class="imgflip" src="img/nekfeu.png" title="Nekfeu" alt="Nekfeu">
+                <img id="rSCH" class="imgflip" src="img/sch.png" title="SCH" alt="SCH">
             </div>
         </div>
     </div>
